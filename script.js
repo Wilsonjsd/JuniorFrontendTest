1daed05c62ca5c65285e256af0463e6d60afa913
Wilsonjsd <sousa.wjd@gmail.com>
2017-04-13 18:00:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+var options = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  }
+}
 
-	
-	var elem = document.getElementById('principal-button');
-	var found = document.getElementsByClassName("notFound");
-	// found.style.diplay = 'none';
-	elem.onClick = function() {
-		var txtArea;
-		document.getElementById("write").value = txtArea;
-			if(txtArea === true){
-				
-			}
-			else{
-				found.style.diplay = 'block';
-			}
-		alert("Hello World");
+function getUserByUsername(username) {
+   fetch('https://api.github.com/users/' + username, options)
+  .then(function(response) {
+    response.statusText()
+    return response.text()
+  })
+  .catch(function(error) {
+    console.log(error)
+  })
+}
+
+var button = document.getElementById('principal-button');
+var notFound = document.getElementsByClassName('notFound');
+
+button.onclick = function() {
+	var user = document.getElementById('write').value;
+
+	console.log(user); // username
+	var user1 = getUserByUsername(user);
+	console.log(user1);
+
+		if(user1) {
+			console.log(user1.name); // nombre completo
+			alert('Hello World');
+		}
+		else {
+			alert('Goodbye World');
+			notFound.style.display ='block';
+		}
 };
 
 
